f78ec68fecc38af112fa5ac7c2662a08a9c63128
Shin Kojima <shin@kojima.org>
2019-08-06 22:43:10 -0700

    improve: Python code does run faster in a function
    see: https://stackoverflow.com/questions/11241523/why-does-python-code-run-faster-in-a-function

--- clear_movie.py
+++ clear_movie.py
@@ -1,39 +1,36 @@
 import unicodedata
 import sys 
 
-in_file = open("2018_movie_raw", "r")
-year = 2018
-month = 0
-day = 0
-for line in in_file.readlines():
-    line = line.strip()
-    if line[len(line) - 1] == '月':
-        month = 0
-        for i in range(len(line) - 1):
-            month = month * 10 + int(line[i:i+1])
-        
-    elif line[len(line) - 1] == '日':
-        day = 0
-        for i in range(len(line) - 1):
-            day = day * 10 + int(line[i:i+1])
-        
-    else:
-        
-        end = len(line)
-        for i in range(len(line)):
+def main():
+    in_file = open("2018_movie_raw", "r")
+    year = 2018
+    month = 0
+    day = 0
+    for line in in_file.readlines():
+        line = line.strip()
+        if line[len(line) - 1] == '月':
+            month = 0
+            for i in range(len(line) - 1):
+                month = month * 10 + int(line[i:i+1])
+            
+        elif line[len(line) - 1] == '日':
+            day = 0
+            for i in range(len(line) - 1):
+                day = day * 10 + int(line[i:i+1])
+            
+        else:
+            
+            end = len(line)
+            for i in range(len(line)):
 
-            if (line[len(line) - i - 1] == '(' or line[len(line) - 1 - i] == '（'):
-                end = len(line) - i - 1
-                break
-        
-        line = line[0:end].strip()
-        line = unicodedata.normalize('NFC', line)
-        print(line, year, month, day)
-                    
-    
-
+                if (line[len(line) - i - 1] == '(' or line[len(line) - 1 - i] == '（'):
+                    end = len(line) - i - 1
+                    break
+            
+            line = line[0:end].strip()
+            line = unicodedata.normalize('NFC', line)
+            print(line, year, month, day)
 
 
-
-    
-
+if __name__ == '__main__':
+    main()
