21eede0dfc8a34e045206dfb5779e5e82608d15f
Shin Kojima <shin@kojima.org>
2019-08-06 22:48:59 -0700

    Use `with` statement to ensure closing opened filehandle

--- clear_movie.py
+++ clear_movie.py
@@ -2,34 +2,35 @@ import unicodedata
 import sys 
 
 def main():
-    in_file = open("2018_movie_raw", "r")
     year = 2018
     month = 0
     day = 0
-    for line in in_file.readlines():
-        line = line.strip()
-        if line[len(line) - 1] == '月':
-            month = 0
-            for i in range(len(line) - 1):
-                month = month * 10 + int(line[i:i+1])
-            
-        elif line[len(line) - 1] == '日':
-            day = 0
-            for i in range(len(line) - 1):
-                day = day * 10 + int(line[i:i+1])
-            
-        else:
-            
-            end = len(line)
-            for i in range(len(line)):
 
-                if (line[len(line) - i - 1] == '(' or line[len(line) - 1 - i] == '（'):
-                    end = len(line) - i - 1
-                    break
-            
-            line = line[0:end].strip()
-            line = unicodedata.normalize('NFC', line)
-            print(line, year, month, day)
+    with open("2018_movie_raw", "r") as in_file:
+        for line in in_file.readlines():
+            line = line.strip()
+            if line[len(line) - 1] == '月':
+                month = 0
+                for i in range(len(line) - 1):
+                    month = month * 10 + int(line[i:i+1])
+                
+            elif line[len(line) - 1] == '日':
+                day = 0
+                for i in range(len(line) - 1):
+                    day = day * 10 + int(line[i:i+1])
+                
+            else:
+                
+                end = len(line)
+                for i in range(len(line)):
+
+                    if (line[len(line) - i - 1] == '(' or line[len(line) - 1 - i] == '（'):
+                        end = len(line) - i - 1
+                        break
+                
+                line = line[0:end].strip()
+                line = unicodedata.normalize('NFC', line)
+                print(line, year, month, day)
 
 
 if __name__ == '__main__':
