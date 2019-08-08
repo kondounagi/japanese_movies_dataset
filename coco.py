fc3439f583ec8d3a0c7d335d77d50d025efbe906
Shin Kojima <shin@kojima.org>
2019-08-07 17:52:02 -0700

    cosme: Fix indent

--- coco.py
+++ coco.py
@@ -20,33 +20,31 @@ def cocoScraping(title):
 
     idTitle = []
     for element in soup.findAll("div",{"class":"li_pp"}):
-            tempId = re.sub(r'\D', '', element.a['href'])
-            tempTitle = element.find('div' , {'class': 'li_ttl'} ).string
-            idTitle.append({'cocoId': tempId , 'title':tempTitle})
-            
-            
+        tempId = re.sub(r'\D', '', element.a['href'])
+        tempTitle = element.find('div' , {'class': 'li_ttl'} ).string
+        idTitle.append({'cocoId': tempId , 'title':tempTitle})
+
     for element in idTitle:
         tempTitle = element['title']
         tempTitle = re.sub(r'\s',' ',tempTitle)
-    
+
         tempTitle = re.sub(r'（[^（）]*）', "", tempTitle)
-    
+
         tempTitle = re.sub(r'\([^()]*\)', "", tempTitle)
         tempTitle = tempTitle.replace('...','')
         element['title'] = tempTitle
-        
+
     regulatedTitle  = re.sub(r'\s',' ',title)
     regulatedTitle  = re.sub(r'（[^（）]*）', "", regulatedTitle)
     regulatedTitle  = re.sub(r'\([^()]*\)', "", regulatedTitle)
     regulatedTitle = regulatedTitle.replace('...','')
-        
+
     select = None
 
     for element in idTitle:
         if regulatedTitle[:len(element['title'])] == element['title']:
             select = element
             break
-            
 
     comments = []
 
@@ -56,12 +54,12 @@ def cocoScraping(title):
         r = http.request('GET', encodedUrl)
         data = r.data.decode('utf-8')
         soup = BeautifulSoup(data,'html.parser')
-        
+
         flag = False
-        
+
         if len(soup.findAll('h2', {'class':'tweet_title2'})) > 0:
             flag = True
-        
+
         li = soup.findAll('li',{'class':'tweet_li'})
         for counter,each in enumerate(li):
             #if (flag == True) and (counter != 0):
@@ -69,8 +67,8 @@ def cocoScraping(title):
             #       break
             comment = each.find('div',{'class':'tweet_text'}).next_element.replace('\n','').replace('\u3000',' ').replace('\r','')
             comments.append(comment)
-            
+
         if flag:
             break
-    
+
     return(comments)
