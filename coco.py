8ae8f45551549d27efba520e86555fe543123b4a
Shin Kojima <shin@kojima.org>
2019-08-07 17:55:43 -0700

    pep8: Fix whitespaces

--- coco.py
+++ coco.py
@@ -15,28 +15,28 @@ def cocoScraping(title):
     r = http.request('GET', url, fields={'q': title})
     data = r.data.decode('utf-8')
 
-    soup = BeautifulSoup(data,'html.parser')
+    soup = BeautifulSoup(data, 'html.parser')
 
     idTitle = []
-    for element in soup.findAll("div",{"class":"li_pp"}):
+    for element in soup.findAll("div", {"class": "li_pp"}):
         tempId = re.sub(r'\D', '', element.a['href'])
-        tempTitle = element.find('div' , {'class': 'li_ttl'} ).string
-        idTitle.append({'cocoId': tempId , 'title':tempTitle})
+        tempTitle = element.find('div', {'class': 'li_ttl'}).string
+        idTitle.append({'cocoId': tempId, 'title': tempTitle})
 
     for element in idTitle:
         tempTitle = element['title']
-        tempTitle = re.sub(r'\s',' ',tempTitle)
+        tempTitle = re.sub(r'\s', ' ', tempTitle)
 
         tempTitle = re.sub(r'（[^（）]*）', "", tempTitle)
 
         tempTitle = re.sub(r'\([^()]*\)', "", tempTitle)
-        tempTitle = tempTitle.replace('...','')
+        tempTitle = tempTitle.replace('...', '')
         element['title'] = tempTitle
 
-    regulatedTitle  = re.sub(r'\s',' ',title)
-    regulatedTitle  = re.sub(r'（[^（）]*）', "", regulatedTitle)
-    regulatedTitle  = re.sub(r'\([^()]*\)', "", regulatedTitle)
-    regulatedTitle = regulatedTitle.replace('...','')
+    regulatedTitle = re.sub(r'\s', ' ', title)
+    regulatedTitle = re.sub(r'（[^（）]*）', "", regulatedTitle)
+    regulatedTitle = re.sub(r'\([^()]*\)', "", regulatedTitle)
+    regulatedTitle = regulatedTitle.replace('...', '')
 
     select = None
 
@@ -48,20 +48,20 @@ def cocoScraping(title):
     comments = []
 
     for i in range(20):
-        url = 'https://coco.to/movie/{}/review/{}'.format(select['cocoId'],str(i+1))
-        encodedUrl = urllib.parse.quote(url,'/:?=&')
+        url = 'https://coco.to/movie/{}/review/{}'.format(select['cocoId'], str(i + 1))
+        encodedUrl = urllib.parse.quote(url, '/:?=&')
         r = http.request('GET', encodedUrl)
         data = r.data.decode('utf-8')
-        soup = BeautifulSoup(data,'html.parser')
+        soup = BeautifulSoup(data, 'html.parser')
 
         flag = False
 
-        if len(soup.findAll('h2', {'class':'tweet_title2'})) > 0:
+        if len(soup.findAll('h2', {'class': 'tweet_title2'})) > 0:
             flag = True
 
-        li = soup.findAll('li',{'class':'tweet_li'})
-        for counter,each in enumerate(li):
-            comment = each.find('div',{'class':'tweet_text'}).next_element.replace('\n','').replace('\u3000',' ').replace('\r','')
+        li = soup.findAll('li', {'class': 'tweet_li'})
+        for counter, each in enumerate(li):
+            comment = each.find('div', {'class': 'tweet_text'}).next_element.replace('\n', '').replace('\u3000', ' ').replace('\r', '')
             comments.append(comment)
 
         if flag:
