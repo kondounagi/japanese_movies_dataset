a5d59800115d6d8d1d63528d3975c3d3fa708324
Shin Kojima <shin@kojima.org>
2019-08-12 11:50:23 -0700

    refactor: Use `bs4.select` with CSS selectors, instead of `bs4.find`

--- eigacom_review/eigacom_review_scraping.py
+++ eigacom_review/eigacom_review_scraping.py
@@ -23,11 +23,11 @@ def search(q):
     res_search = requests.get(url_search)
     res_search.encoding = res_search.apparent_encoding
 
-    soup_search = BeautifulSoup(res_search.content, "lxml")
-    result =  soup_search.find('section', attrs={"id": "rslt-movie"})
+    soup = BeautifulSoup(res_search.content, "lxml")
+    result = soup.select_one('section#rslt-movie')
 
     if result:
-        path = result.find('li', attrs={"class": "col-s-3"}).find('a')["href"]
+        path = result.select_one('a[href^="/movie/"]')['href']
         url_review = 'https://eiga.com' + path + 'review/all/'
         return url_review
     else:
@@ -57,18 +57,24 @@ def scrape_review(query):
         res = requests.get(url_review + str(page_num))
         res.encoding = res.apparent_encoding
         soup = BeautifulSoup(res.content, "lxml")
-        if soup.find('div', attrs={"class": "user-review"}) == None: # ページ数の上限を超えたら
+        reviews = soup.select('div.user-review')
+
+        # ページ数の上限を超えたら
+        if not reviews:
             print('DONE : ' + query )
             break
 
-        for r in soup.find_all('div', attrs={"class": "user-review"}):
-            title = r.find('h2',attrs={"class": "review-title"}).find('a')
-            main_text =  r.find('div',attrs={"class": "txt-block"})
-            tgl_btn = main_text.find('div',attrs={"class": "toggle-btn"})
-            if tgl_btn is not None:
+        for r in reviews:
+            title = r.select_one('h2.review-title a')
+            main_text = r.select_one('div.txt-block')
+            tgl_btn = main_text.select_one('div.toggle-btn')
+
+            if tgl_btn:
                 tgl_btn.decompose()
+
             d = title.text + "\n" +  main_text.text.replace("\n", "")
             data["reviews"]["eigacom"].append(d)
+
         page_num += 1
         time.sleep(1)
     return data
