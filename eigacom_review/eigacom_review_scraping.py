6830143b7ddd9b17006c08a46c01473d8fce675d
Shin Kojima <shin@kojima.org>
2019-08-11 17:12:03 -0700

    cosme: Remove unused flag re.U
    See: https://docs.python.org/3/library/re.html#re.ASCII
    
    > Note that for backward compatibility, the re.U flag still exists (as
    > well as its synonym re.UNICODE and its embedded counterpart (?u)), but
    > these are redundant in Python 3 since matches are Unicode by default for
    > strings (and Unicode matching isn't allowed for bytes).

--- eigacom_review/eigacom_review_scraping.py
+++ eigacom_review/eigacom_review_scraping.py
@@ -8,7 +8,7 @@ import json
 def search(q):
     q = q.replace('\n', '').replace('（', '(').replace('）', ')')
     print("START : " + q)
-    q = re.sub("\(.+\)$", "", " ".join(q), re.UNICODE)
+    q = re.sub("\(.+\)$", "", " ".join(q))
     query = re.sub('(!|\u3000|/|\s|>|<|\.)+', "%20", q)
     url_search = 'https://eiga.com/search/' + query
     res_search = requests.get(url_search )
