12b6b6001028aa486ee282c23cef78a8c33e26c5
Shin Kojima <shin@kojima.org>
2019-08-12 17:23:08 -0700

    refactor: wrap with `main()`

--- title2json.py
+++ title2json.py
@@ -2,14 +2,20 @@
 import csv
 import json
 
-with open("2018_movie_clean", 'r') as src:
-    for line in csv.reader(src, delimiter='\t'):
-        num, name, *_ = line
 
-        with open("movies/{}.json".format(num), 'r+') as f:
-            data = json.load(f)
-            data['title'] = name
-            f.seek(0)
-            json.dump(data, f, indent=2, ensure_ascii=False)
-            f.write('\n')
-            f.truncate()
+def main():
+    with open("2018_movie_clean", 'r') as src:
+        for line in csv.reader(src, delimiter='\t'):
+            num, name, *_ = line
+
+            with open("movies/{}.json".format(num), 'r+') as f:
+                data = json.load(f)
+                data['title'] = name
+                f.seek(0)
+                json.dump(data, f, indent=2, ensure_ascii=False)
+                f.write('\n')
+                f.truncate()
+
+
+if __name__ == '__main__':
+    main()
