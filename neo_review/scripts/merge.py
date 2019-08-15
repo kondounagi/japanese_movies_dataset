2e513789eda4989f9ccb76fa468a491c360bbf25
Shin Kojima <shin@kojima.org>
2019-08-14 21:52:33 -0700

    pep8: Fix whitespaces

--- neo_review/scripts/merge.py
+++ neo_review/scripts/merge.py
@@ -2,11 +2,12 @@ import json
 import os
 import sys
 
+
 def main(meta_filepath, input_dir, output_filepath):
     meta_data = None
     with open(meta_filepath, 'r', encoding='utf-8') as f:
         meta_data = json.load(f)
-    
+
     output_dict = {}
 
     for year in meta_data.keys():
@@ -15,9 +16,10 @@ def main(meta_filepath, input_dir, output_filepath):
             with open(input_dir.rstrip('/') + '/{}.json'.format(nomination_id), 'r', encoding='utf-8') as f:
                 data = json.load(f)
                 output_dict.update(data)
-    
+
     with open(output_filepath, 'w', encoding='utf-8') as output:
         json.dump(output_dict, output, indent=4, ensure_ascii=False)
+
 
 if __name__ == '__main__':
     args = sys.argv
@@ -25,4 +27,4 @@ if __name__ == '__main__':
         print('disignate filepath')
         sys.exit(0)
     if len(args) == 4:
-        main(args[1],args[2],args[3])
+        main(args[1], args[2], args[3])
