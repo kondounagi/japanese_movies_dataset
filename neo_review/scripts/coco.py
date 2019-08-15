81bdfe85d2176fa6cd83bf5cbb4aa7688189c256
Ruofan He <hrinternship2018@gmail.com>
2019-08-14 20:03:48 -0700

    Add files to get coco reviews with date

---
+++ neo_review/scripts/coco.py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python3
+import certifi
+import urllib3
+import urllib
+from bs4 import BeautifulSoup
+import re
+import csv
+import sys
+import json
+import os
+
+def main(filepath: str, output_dir: str, year):
+    meta_data = None
+    with open(filepath, 'r', encoding= 'utf-8') as f:
+        meta_data = json.load(f)
+    
+    for element in meta_data[year]:
+        output_dict = {}
+        nomination_id = element['id']
+        title = element['title']
+        select = getCocoId(title)
+        reviews = getCocoReview(select)
+        temp_dict = {'title': title, 'reviews': reviews}
+        output_dict[str(nomination_id)] = temp_dict
+        os.makedirs(output_dir, exist_ok=True)
+        output = open(output_dir.rstrip('/') + '/{}.json'.format(nomination_id), 'w', encoding='utf-8')
+        json.dump(output_dict , output, indent=4, ensure_ascii=False)
+        output.write('\n')
+        output.close()
+
+    return
+
+def getCocoId(title):
+    def trans(title_string):
+        table = str.maketrans({
+            '１': '1',
+            '２': '2',
+            '３': '3',
+            '４': '4',
+            '５': '5',
+            '６': '6',
+            '７': '7',
+            '８': '8',
+            '９': '9',
+            '０': '0',
+            '＆': '&',
+            '％': '%',
+            '＝': '=',
+            '＄': '$',
+            '＃': '#',
+            '！': '!',
+            '？': '?'
+        })
+        regulated_title = re.sub(r'（[^（）]*）', '', title_string)
+        regulated_title = re.sub(r'\([^\(\)]*\)', '', regulated_title)
+        regulated_title = re.sub(r'[\s\-～〜:：;、。<>＜＞「」\"\',\.・/／－]+', ' ', regulated_title)
+        regulated_title = regulated_title.translate(table)
+        regulated_title = regulated_title.rstrip(' ')
+        return regulated_title
+
+    http = urllib3.PoolManager(
+        cert_reqs='CERT_REQUIRED',
+        ca_certs=certifi.where())
+
+    url = 'https://coco.to/movies'
+
+    regulated_title = trans(title)
+
+    r = http.request('GET', url, fields={'q': regulated_title})
+    data = r.data.decode('utf-8')
+
+    soup = BeautifulSoup(data, 'html.parser')
+
+    id_title = []
+    for element in soup.findAll("div", {"class": "li_pp"}):
+        temp_id = re.sub(r'\D', '', element.a['href'])
+        temp_title = element.find('div', {'class': 'li_ttl'}).string
+        id_title.append({'cocoId': temp_id, 'title': temp_title})
+
+    for element in id_title:
+        temp_title = element['title']
+        temp_title = trans(temp_title)
+        temp_title = temp_title.replace('...', '')
+        element['title'] = temp_title
+
+    regulated_title = trans(title)
+    regulated_title = regulated_title.replace('...', '')
+
+    select = None
+
+    for element in id_title:
+        min_length = min(len(element['title']), len(regulated_title))
+        if regulated_title[:min_length] == element['title'][:min_length]:
+            select = element
+            break
+
+    if select is None and len(id_title) is not 0:
+        select = id_title[0]
+
+    return select
+
+def getCocoReview(select):
+    http = urllib3.PoolManager(
+        cert_reqs='CERT_REQUIRED',
+        ca_certs=certifi.where())
+
+    comments = []
+    if select is None:
+        return comments
+
+    for i in range(200):
+        url = 'https://coco.to/movie/{}/review/{}'.format(select['cocoId'], str(i + 1))
+        encoded_url = urllib.parse.quote(url, '/:?=&')
+        r = http.request('GET', encoded_url)
+        data = r.data.decode('utf-8')
+        soup = BeautifulSoup(data, 'html.parser')
+
+        flag = False
+
+        if len(soup.findAll('h2', {'class': 'tweet_title2'})) > 0:
+            flag = True
+
+        if len(soup.findAll('h2', {'class': 'tweet_title1'})) > 0:
+            flag = True
+
+        li = soup.findAll('li', {'class': 'tweet_li'})
+
+        if len(li) == 0:
+            flag = True
+
+        for counter, each in enumerate(li):
+            comment_string = each.find('div', {'class': 'tweet_text'}).next_element
+            processed_comment = re.sub(r'\s', ' ', comment_string)
+            date_element = each.find('div', {'class': 'updated'})
+            date_string = date_element.a['title']
+            date_string = re.search(r'\d\d\d\d:\d\d:\d\d',date_string).group().replace(':','-')
+            comments.append({'date': date_string, 'review': processed_comment})
+
+        if flag:
+            break
+
+    return(comments)
+
+if __name__ == '__main__':
+    args = sys.argv
+    if len(args) < 4:
+        print('disignate filepath')
+        sys.exit(0)
+    if len(args) == 4:
+        main(args[1],args[2],args[3])
---
+++ neo_review/scripts/merge.py
@@ -0,0 +1,28 @@
+import json
+import os
+import sys
+
+def main(meta_filepath, input_dir, output_filepath):
+    meta_data = None
+    with open(meta_filepath, 'r', encoding='utf-8') as f:
+        meta_data = json.load(f)
+    
+    output_dict = {}
+
+    for year in meta_data.keys():
+        for element in meta_data[year]:
+            nomination_id = element['id']
+            with open(input_dir.rstrip('/') + '/{}.json'.format(nomination_id), 'r', encoding='utf-8') as f:
+                data = json.load(f)
+                output_dict.update(data)
+    
+    with open(output_filepath, 'w', encoding='utf-8') as output:
+        json.dump(output_dict, output, indent=4, ensure_ascii=False)
+
+if __name__ == '__main__':
+    args = sys.argv
+    if len(args) < 4:
+        print('disignate filepath')
+        sys.exit(0)
+    if len(args) == 4:
+        main(args[1],args[2],args[3])
