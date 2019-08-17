d40f49b4f5a80ba797490a2035e0b97a9ae66cb7
Shin Kojima <shin@kojima.org>
2019-08-17 15:54:27 -0700

    bugfix: clean_paren

--- clear_movie.py
+++ clear_movie.py
@@ -34,6 +34,33 @@ def parse_arg():
     return year, filename
 
 
+def clean_paren(text):
+    """Remove nested parentheses from text.
+
+    >>> clean_paren( 'アンダー・ザ・ウォーター（ スウェーデン・ デンマーク・ フィンランド）')
+    'アンダー・ザ・ウォーター'
+
+    >>> clean_paren( '葡萄畑に帰ろう (ジョージア (国)の旗 ジョージア)')
+    '葡萄畑に帰ろう '
+
+    >>> clean_paren('(r)adius ラディウス（英語版）')  # OMG
+    'adius ラディウス'
+    """
+    left_parenthese = 0
+    right_parenthese = 0
+
+    saved = []
+    for char in text:
+        if char == "(" or char == "（":
+            left_parenthese += 1
+        elif char == ")" or char == "）":
+            right_parenthese += 1
+        elif right_parenthese >= left_parenthese:
+            saved.append(char)
+
+    return ''.join(saved)
+
+
 @contextlib.contextmanager
 def open_resource(filename):
     if filename:
@@ -54,9 +81,7 @@ def main(year, filename):
 
     re_month = re.compile('^([0-9]+)月$')
     re_day = re.compile('^([0-9]+)日$')
-    re_image_desc = re.compile(r'\s*[(（][^)）]*の旗[^)）]*[)）]')
     re_trailing_num = re.compile(r'(?:\s*\[[0-9]+\])+$')
-    re_garbage = re.compile(r'\s*[(（][^)）]*$')
 
     num = 0
     with open_resource(filename) as in_file:
@@ -73,11 +98,8 @@ def main(year, filename):
                 day = match_day.group(1)
                 continue
 
-            # FIXME: Regexp can't handle nested parens, need stack parser
-            line = re_image_desc.sub('', line)
-
+            line = clean_paren(line)
             line = re_trailing_num.sub('', line)
-            line = re_garbage.sub('', line)
 
             line = unicodedata.normalize('NFC', line)
 
