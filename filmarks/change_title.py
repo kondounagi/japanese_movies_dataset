19c80e2ebcdf9caf29944ce8b745cc84bba16a09
Shin Kojima <shin@kojima.org>
2019-08-18 10:25:20 -0700

    cosme: Use f-string literal(Python 3.6+)

--- filmarks/change_title.py
+++ filmarks/change_title.py
@@ -4,20 +4,19 @@ import os
 
 
 year = int(sys.argv[1])
-os.makedirs('./jsons/{}'.format(year), exist_ok=True)
+os.makedirs(f'./jsons/{year}', exist_ok=True)
 
-with open('../{}_movie_clean'.format(year), 'r') as f:
+with open(f'../{year}_movie_clean', 'r') as f:
     data_reader = csv.reader(f, delimiter='\t')
     for row in data_reader:
         movie_id = row[0]
         title = row[1]
         # print(movie_id, title)
-        print('./jsons/metadata/{}/{}.json'.format(year, title))
+        src = f'./jsons/metadata/{year}/{title}.json'
+        dst = f'./jsons/{year}/{movie_id}.json'
+        print(src)
         try:
-            os.rename(
-                './jsons/metadata/{}/{}.json'.format(year, title),
-                './jsons/{}/{}.json'.format(year, movie_id),
-            )
+            os.rename(src, dst)
         except Exception as e:
             print(e)
             # pass
