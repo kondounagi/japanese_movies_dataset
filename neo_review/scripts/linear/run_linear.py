cd8a49859c34bb00e65ade22f573eb086400e05a
Zhenchao Lin <cszclin@gmail.com>
2019-08-21 14:23:13 -0700

    Fix and train with shared pkl

--- neo_review/scripts/linear/run_linear.py
+++ neo_review/scripts/linear/run_linear.py
@@ -1,128 +1,69 @@
 #!/usr/bin/env python3
-""" Run SVM
+""" Run Linear Regression
 """
 import math
 import numpy as np
 import pandas as pd
-from sklearn import svm
+from sklearn.linear_model import LinearRegression
 from sklearn import metrics
 import optuna
-from sklearn.neural_network import MLPRegressor
-from sklearn.linear_model import LinearRegression
 
 
 def read_in_data():
-    """ Read in training data and return in np format
+    """ Read in training data and return
 
     Args:
         NA
 
     Returns:
-        data:
-        meta_data_X:
-        meta_data_y:
+        years: a list of year indicated
+        data_X:
+        data_y:
     """
-    data = pd.read_pickle("data.pkl")
-
-    nomination_onehot = (
-        pd.read_pickle("nomination_onehot.pkl"))
-    selected_performers_onehot = (
-        pd.read_pickle("selected_performers_onehot.pkl"))
-    selected_directors_onehot = (
-        pd.read_pickle("selected_directors_onehot.pkl"))
-    selected_studio_onehot = (
-        pd.read_pickle("selected_studio_onehot.pkl"))
-    selected_scriptwriter_onehot = (
-        pd.read_pickle("selected_scriptwriter_onehot.pkl"))
-
-    bundle = [
-        nomination_onehot,
-        selected_performers_onehot,
-        selected_directors_onehot,
-        selected_studio_onehot,
-        selected_scriptwriter_onehot,
-    ]
-
-    meta_data_X = pd.concat(bundle, axis=1)
-    meta_data_y = data["prize"]
-    meta_data_X = np.array(meta_data_X)
-    meta_data_y = np.array(meta_data_y)
-    return data, meta_data_X, meta_data_y
-
+    data = pd.read_pickle("unificated_data_set.pkl")
+    years = data["year"]
+    data_y = data["prize"]
+    data_X = data.drop(["year", "prize"], axis=1)
+    return years, data_X, data_y
 
 def calculate_auc(test, pred):
     fpr, tpr, _ = metrics.roc_curve(test, pred)
     return metrics.auc(fpr, tpr)
 
+def train(params):
+    years, data_X, data_y = read_in_data()
+    pred_y = pd.DataFrame()
+    ans_y = pd.DataFrame()
+
+    model = LinearRegression(
+        fit_intercept=params["linear_fit_intercept"],
+        normalize=params["linear_normalize"]
+    )
+
+    for year in range(1978, 2020):
+        train_X = data_X[years != year]
+        train_y = data_y[years != year]
+        test_X = data_X[years == year] 
+        test_y = data_y[years == year]
+
+        model.fit(train_X, train_y)
+        pred_y = pd.concat([pred_y, pd.DataFrame(model.predict(test_X))])
+        ans_y = pd.concat([ans_y, pd.DataFrame(test_y)])
+    
+    return ans_y, pred_y
 
 def objective(trail):
     """ Optuna objective parameter tuning function
     """
-    linear_fit_intercept = (
-        trail.suggest_categorical("linear_fit_intercept", [True, False]))
-    linear_normalize = (
-        trail.suggest_categorical("linear_normalize", [True, False]))
-
+    linear_fit_intercept = trail.suggest_categorical("linear_fit_intercept", [True, False])
+    linear_normalize = trail.suggest_categorical("linear_normalize", [True, False])
     params = {
         "linear_fit_intercept": linear_fit_intercept,
-        "linear_normalize": linear_normalize,
+        "linear_normalize": linear_normalize
     }
 
-    all_test_y, all_pred_y = run_linear_with_param(params)
-    return -calculate_auc(all_test_y, all_pred_y)
-
-
-def run_linear_with_param(param):
-    all_test_y = []
-    all_pred_y = []
-    data, meta_data_X, meta_data_y = read_in_data()
-
-    true_positive = 0
-    true_negative = 0
-    false_positive = 0
-    false_negative = 0
-    for year in range(1978, 2020):
-        train_X = []
-        test_X = []
-        train_y = []
-        test_y = []
-        titles = []
-        for i in range(len(data)):
-            if int(data["year"][i + 1]) != year:
-                train_X.append(meta_data_X[i])
-                train_y.append(meta_data_y[i])
-            else:
-                test_X.append(meta_data_X[i])
-                test_y.append(meta_data_y[i])
-                if data["prize"][i + 1] == 1:
-                    titles.append("☆" + data["title"][i + 1])
-                else:
-                    titles.append(data["title"][i + 1])
-
-        regression_clf = LinearRegression(
-            fit_intercept=param["linear_fit_intercept"],
-            normalize=param["linear_normalize"],
-        )
-        regression_clf.fit(train_X, train_y)
-        regression_res = regression_clf.predict(test_X)
-        '''
-        print(year)
-        for i in range(len(regression_res)):
-            print(titles[i], regression_res[i])
-            if list(regression_res.data).index(max(regression_res)) == i:
-                if titles[i][0] == "☆":
-                    true_positive += 1
-                else:
-                    false_positive += 1
-            else:
-                if titles[i][0] == "☆":
-                    true_negative += 1
-                else:
-                    false_negative += 1
-        '''
-        all_pred_y += regression_res.data
-        all_test_y += test_y
-    return all_test_y, all_pred_y
+    test_y, pred_y = train(params)
+    return -calculate_auc(test_y, pred_y)
 
 
 def main():
@@ -137,15 +78,14 @@ def main():
     study = optuna.create_study()
     study.optimize(objective, n_trials=10)
     # best param after 10 trainings
-    # best auc 0.6424344885883347
+    # auc = 0.6699774584389969
     param = {
         "linear_fit_intercept": True,
-        "linear_normalize": True,
+        "linear_normalize": False
     }
     param = study.best_params
-    test_y, pred_y = run_linear_with_param(param)
+    test_y, pred_y = train(param)
     print(calculate_auc(test_y, pred_y))
-
 
 if __name__ == "__main__":
     main()
