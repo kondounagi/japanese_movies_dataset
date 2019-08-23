b1a11080c1579bf3ac34e15bd5ec9710c0b72742
okuyama <momijifumiwake0206@gmail.com>
2019-08-23 10:57:26 -0700

    Add NN model README.md

--- neo_review/scripts/nn/neural_network_model.py
+++ neo_review/scripts/nn/neural_network_model.py
@@ -10,7 +10,7 @@ from utility import Utility
 
 
 class NeuralNetworkModel(Chain):
-    def __init__(self, n_l0=25, n_l1=122, n_l2=5, n_l3=71, n_l4=72, n_l5=123, n_l6=37, n_l7=85, n_l8=108, n_out=1):
+    def __init__(self, n_l0=121, n_l1=37, n_l2=5, n_l3=71, n_l4=72, n_l5=123, n_l6=37, n_l7=85, n_l8=108, n_out=1):
         self._n_out = n_out
 
         super().__init__()
@@ -56,7 +56,7 @@ class NeuralNetworkModel(Chain):
         h5 = F.relu(self._l5(h4))
         h5 = self._b5(h5)
 
-        hl = self._ll(h5)
+        hl = self._ll(h1)
         return hl
 
     def run_as_script(self):
