244e34587268a954f495a41831ebd7d373119323
Shin Kojima <shin@kojima.org>
2019-08-23 08:55:02 -0700

    F401 imported but unused

--- neo_review/scripts/linear/run_linear.py
+++ neo_review/scripts/linear/run_linear.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 """ Run Linear Regression
 """
-import math
-import numpy as np
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn import metrics
