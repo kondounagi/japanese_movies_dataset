0bc0301de6995966a123774aec93b9aa611604e1
Shin Kojima <shin@kojima.org>
2019-08-23 16:53:42 -0700

    Add fetch subcommand

--- src/nay/__init__.py
+++ src/nay/__init__.py
@@ -1,3 +1,15 @@
+import argparse
+
+
 def main():
     """Entry point for the application script"""
-    print("Call your main application code here")
+    parser = argparse.ArgumentParser(description="nay")
+
+    parser.add_argument('subcommand',
+                        help='[fetch]',
+                        type=str)
+
+    args = parser.parse_args()
+
+    if args.subcommand == 'fetch':
+        print("TODO: fetch'em all!")
