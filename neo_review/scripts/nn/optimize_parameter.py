b31d411750b9eec752e7e8473926cdb7ab351cb0
Shin Kojima <shin@kojima.org>
2019-08-23 09:54:15 -0700

    cosme: Dismiss flake8 error: F/L

--- neo_review/scripts/nn/optimize_parameter.py
+++ neo_review/scripts/nn/optimize_parameter.py
@@ -1,8 +1,8 @@
 import argparse
 
 import chainer
-import chainer.functions as F
-import chainer.links as L
+import chainer.functions as F  # noqa: N812
+import chainer.links as L  # noqa: N812
 from chainer import training, Chain
 from chainer.training import extensions
 
