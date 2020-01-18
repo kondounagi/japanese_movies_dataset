22309e57ce3e45d0d8073b8b72f20ece8448d8dd
Shin Kojima <shin@kojima.org>
2020-01-18 11:52:59 +0900

    bugfix: Fix filename typo

--- neo_review/scripts/download.sh
+++ neo_review/scripts/download.sh
@@ -4,5 +4,5 @@ python ./coco.py \
     ../data/coco \
     $(seq 1978 2020)
 
-python ./mearge.py ../data/nominate_movie_meta_data.json ../data/coco ../data/coco_reviews.json
+python ./merge.py ../data/nominate_movie_meta_data.json ../data/coco ../data/coco_reviews.json
 rm -rf ../data/coco
