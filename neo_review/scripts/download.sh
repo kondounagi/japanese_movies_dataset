713b4266596916bc39bd6a25f108841c2d46f8f3
Shin Kojima <shin@kojima.org>
2021-03-03 18:45:32 +0900

    bugfix: Get end_year from current year

--- neo_review/scripts/download.sh
+++ neo_review/scripts/download.sh
@@ -2,7 +2,7 @@
 python ./coco.py \
     ../data/nominate_movie_meta_data.json \
     ../data/coco \
-    $(seq 1978 2020)
+    $(seq 1978 $(date +%Y))
 
 python ./merge.py ../data/nominate_movie_meta_data.json ../data/coco ../data/coco_reviews.json
 rm -rf ../data/coco
